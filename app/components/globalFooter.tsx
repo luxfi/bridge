89a5d13003c8a189747fcf723a4aa39bac3c2572
Zach Kelling <z@zeekay.io>
2023-12-31 01:38:57 +0100

    Update global footer

--- app/components/globalFooter.tsx
+++ app/components/globalFooter.tsx
@@ -11,35 +11,35 @@ const GLobalFooter = () => {
             { name: 'Product', href: '/' },
             { name: 'Company', href: '/company' },
             { name: 'FAQ', href: '/faq' },
-            { name: 'Privacy Policy', href: 'https://docs.bridge.lux.network/information/privacy-policy' },
-            { name: 'Terms of Services', href: 'https://docs.bridge.lux.network/information/terms-of-services' },
-            { name: 'Docs', href: 'https://docs.bridge.lux.network/onboarding-sdk/' },
+            { name: 'Privacy Policy', href: 'https://lux.partners/privacy' },
+            { name: 'Terms of Services', href: 'https://lux.partners/terms' },
+            { name: 'Docs', href: 'https://docs.bridge.lux.network' },
         ],
         social: [
             {
                 name: 'Twitter',
-                href: 'https://twitter.com/bridge',
+                href: 'https://twitter.com/luxdefi',
                 icon: () => (
                     <TwitterLogo className="h-6 w-6" aria-hidden="true" />
                 ),
             },
             {
                 name: 'Discord',
-                href: 'https://discord.gg/bridge',
+                href: 'https://discord.gg/XsD63KMbV2',
                 icon: () => (
                     <DiscordLogo className="h-6 w-6" aria-hidden="true" />
                 ),
             },
             {
                 name: 'GitHub',
-                href: 'https://github.com/bridge/bridgeapp',
+                href: 'https://github.com/luxdefi',
                 icon: () => (
                     <GitHubLogo className="h-6 w-6" aria-hidden="true" />
                 ),
             },
             {
                 name: 'YouTube',
-                href: 'https://www.youtube.com/@bridgehq',
+                href: 'https://www.youtube.com/@luxdefi',
                 icon: () => (
                     <YoutubeLogo className="h-6 w-6" aria-hidden="true" />
                 ),
@@ -51,10 +51,10 @@ const GLobalFooter = () => {
         <footer className="z-0 hidden md:flex fixed bottom-0 py-4 justify-between items-center w-full px-6 lg:px-8 mt-auto">
             <div>
                 <div className="flex mt-3 md:mt-0 gap-6">
-                    <Link target="_blank" href="https://docs.bridge.lux.network/information/privacy-policy" className="text-xs leading-6 text-primary-text-muted underline hover:no-underline hover:text-opacity-70 duration-200 transition-all">
+                    <Link target="_blank" href="https://lux.partners/privacy" className="text-xs leading-6 text-primary-text-muted underline hover:no-underline hover:text-opacity-70 duration-200 transition-all">
                         Privacy Policy
                     </Link>
-                    <Link target="_blank" href="https://docs.bridge.lux.network/information/terms-of-services" className="text-xs leading-6 text-primary-text-muted underline hover:no-underline hover:text-opacity-70 duration-200 transition-all">
+                    <Link target="_blank" href="https://lux.partners/terms" className="text-xs leading-6 text-primary-text-muted underline hover:no-underline hover:text-opacity-70 duration-200 transition-all">
                         Terms of Services
                     </Link>
                 </div>
