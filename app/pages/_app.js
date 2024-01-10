a643c010891b6c389d3db82ebada164c73f38cc9
artemis prime <artemisprimedev@gmail.com>
2024-01-10 05:45:45 -0800

    Restored Lux Fonts

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,15 +1,25 @@
-import '../styles/globals.css'
-import '../styles/dialog-transition.css'
 import { useRouter } from "next/router";
 import { IntercomProvider } from 'react-use-intercom';
 import { SWRConfig } from 'swr'
+
+import { inter, drukTextWide } from '@luxdefi/ui/style/nextFonts'
+
+import '../styles/globals.css'
+import '../styles/dialog-transition.css'
+
 
 const INTERCOM_APP_ID = 'h5zisg78'
 import "@rainbow-me/rainbowkit/styles.css";
 
 function App({ Component, pageProps }) {
   const router = useRouter()
-  return (
+  return (<>
+    <style jsx global>{`
+        html {
+          --font-inter: ${inter.style.fontFamily};
+          --font-druk-text-wide: ${drukTextWide.style.fontFamily};
+        }
+      `}</style>
     <SWRConfig
       value={{
         revalidateOnFocus: false,
@@ -18,7 +28,8 @@ function App({ Component, pageProps }) {
       <IntercomProvider appId={INTERCOM_APP_ID} initializeDelay={2500}>
         <Component key={router.asPath} {...pageProps} />
       </IntercomProvider>
-    </SWRConfig>)
+    </SWRConfig>
+  </>)
 }
 
 export default App
