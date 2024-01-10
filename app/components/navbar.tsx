95e52033f445aa81e8e0d482663f4cb331b7bca9
artemis prime <artemisprimedev@gmail.com>
2024-01-10 15:11:09 -0800

    added logo

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import GoHomeButton from './utils/GoHome';
 
-export default function Navbar() {
+const Navbar: React.FC = () => (
+  <div className='mt-12 mb-8 mx-auto px-4 overflow-hidden hidden md:block'>
+    <div className="flex justify-center">
+      <GoHomeButton className='h-11 w-auto cursor-pointer' />
+    </div>
+  </div>
+)
 
-    return (
-        <div className='mt-12 mb-8 mx-auto px-4 overflow-hidden hidden md:block'>
-            <div className="flex justify-center">
-                <GoHomeButton className='h-11 w-auto text-primary-logoColor fill-primary-text cursor-pointer'/>
-            </div>
-        </div>
-    )
-}
+export default Navbar
+
+
