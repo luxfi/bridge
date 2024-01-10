284bf28dd56df718c6e086a31450dff79380923f
artemis prime <artemisprimedev@gmail.com>
2024-01-10 05:45:45 -0800

    Restored Lux Fonts

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}", 
     "./components/**/*.{js,ts,jsx,tsx}", 
-    '../ui/**/*.tsx',
+    '../ui/**/*.{ts, tsx}',
     './styles/nextFonts.ts'
   ],
   darkMode: ["class"],
