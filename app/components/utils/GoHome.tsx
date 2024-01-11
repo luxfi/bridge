b1c6e96b26f8a37e613f367492e1ccd1e40fb2b5
artemis prime <artemisprimedev@gmail.com>
2024-01-10 20:38:25 -0800

    and more skinning

--- app/components/utils/GoHome.tsx
+++ app/components/utils/GoHome.tsx
@@ -27,16 +27,15 @@ const GoHomeButton: FC<{
         <ContextMenuPrimitive.Trigger>
           <Logo size='md' className="text-primary-lux"/>
         </ContextMenuPrimitive.Trigger>
-        <ContextMenuPrimitive.Content className="dialog-overlay absolute z-40 border h-fit text-secondary-text border-secondary-100 mt-2 w-fit rounded-md shadow-lg bg-secondary-900 ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <ContextMenuPrimitive.ContextMenuItem className="dialog-content px-4 py-2 text-sm text-left w-full rounded-t hover:bg-secondary-400 whitespace-nowrap">
+        <ContextMenuPrimitive.Content className="dialog-overlay absolute z-40 border h-fit text-muted border-muted-2 mt-2 w-fit rounded-md shadow-lg bg-level-1 focus:outline-none">
+          <ContextMenuPrimitive.ContextMenuItem className="dialog-content px-4 py-2 text-sm text-left w-full rounded-t hover:bg-level-2 whitespace-nowrap">
             <CopyButton toCopy={renderToString(<BridgeLogo />)}>Copy logo as SVG</CopyButton>
           </ContextMenuPrimitive.ContextMenuItem >
-          <ContextMenuPrimitive.ContextMenuItem className="dialog-content px-4 py-2 text-sm text-left w-full hover:bg-secondary-400 whitespace-nowrap">
+          <ContextMenuPrimitive.ContextMenuItem className="dialog-content px-4 py-2 text-sm text-left w-full hover:bg-level-2 whitespace-nowrap">
             <CopyButton toCopy={renderToString(<BridgeLogoSmall />)}>Copy symbol as SVG</CopyButton>
           </ContextMenuPrimitive.ContextMenuItem >
-          <hr className="horizontal-gradient" />
           <ContextMenuPrimitive.ContextMenuItem className="dialog-content">
-            <a href="" target='_blank' className='flex space-x-1 items-center px-4 py-2 rounded-b text-sm text-left w-full hover:bg-secondary-400 whitespace-nowrap'>
+            <a href="" target='_blank' className='flex space-x-1 items-center px-4 py-2 rounded-b text-sm text-left w-full hover:bg-level-2 whitespace-nowrap'>
               <Paperclip width={16} />
               <p>Brand Guidelines</p>
             </a>
