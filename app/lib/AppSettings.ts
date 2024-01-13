97f281ec3d97eb5f8e2d09772bfcca80055f8fa1
Zach Kelling <z@zeekay.io>
2024-01-13 04:58:30 +0100

    Fix explorer link

--- app/lib/AppSettings.ts
+++ app/lib/AppSettings.ts
@@ -1,6 +1,6 @@
 export default class AppSettings {
     static BridgeApiUri?: string = process.env.NEXT_PUBLIC_LS_BRIDGE_API;
     static ApiVersion: string = process.env.NEXT_PUBLIC_API_VERSION || 'mainnet';
-    static ExplorerURl: string = `https://bridge.lux.network/${process.env.NEXT_PUBLIC_API_VERSION === 'sandbox' ? 'testnet/' : ''}explorer`
+    static ExplorerURl: string = `https://explore.bridge.lux${process.env.NEXT_PUBLIC_API_VERSION === 'sandbox' ? '-test' : ''}.network`
 }
 
