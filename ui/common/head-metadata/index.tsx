901e50a0c872d4081ad519de2f878e7448d34477
artemis prime <artemisprimedev@gmail.com>
2024-01-17 17:44:27 -0800

    working metadata

--- ui/common/head-metadata/index.tsx
+++ ui/common/head-metadata/index.tsx
@@ -2,45 +2,46 @@ import React from 'react'
 
 import type { Metadata } from 'next'
 
-import {
+import type {
   IconDescriptor,
   TemplateString,
   Author,
   ThemeColorDescriptor
-} from './next-metadata-types'
+} from './from-next/metadata-types'
+
+import type { OpenGraph, OGImage } from './from-next/opengraph-types'
+import type { Twitter, TwitterImage } from './from-next/twitter-types'
+
+const getURLasString = (url: string | URL) => {
+  return (
+    (typeof url === 'string') ? (url as string) : (url.href)  
+  )
+}
 
 const Icons: React.FC<{icons: IconDescriptor[]}> = ({
   icons
 }) => {
   return <>
   {icons.map(({url, ...rest}: IconDescriptor, index) => (
-    <link {...rest} href={
-      (typeof url === 'string') ? (url as string) : (url.href)
-    }/>
-  )}
+    <link {...rest} href={getURLasString(url)} key={`icons-${index}`}/>
+  ))}
   </>
 }
 
-const Title: React.FC<{title: string | TemplateString | null | undefined}> = ({
-  title
-}) => {
+export const getTitleFromTemplateString = (title: string | TemplateString | null | undefined): string | null  => {
 
   if (!title) {
     return null
   }
   if (typeof title === 'object') {
     if ('default' in title) {
-      return (
-        <title>{title.default}</title>
-      )
+      return  title.default
     }
     else if ('absolute' in title) {
-      return (
-        <title>{title.absolute}</title>
-      )
+      return title.absolute
     }
   }
-  return <title>{title as string}</title>
+  return title as string
 }
 
 const Authors: React.FC<{
@@ -51,9 +52,7 @@ const Authors: React.FC<{
 
   const Author: React.FC<{author: Author}> = ({author}) => (<>
     {author.name && <meta name="author" content={author.name}  />}
-    {author.url && <link rel="author" href={
-      (typeof author.url === 'string') ? (author.url as string) : (author.url.href)
-    }/>}
+    {author.url && <link rel="author" href={getURLasString(author.url)}/>}
   </>)
 
   if (!authors) {
@@ -118,10 +117,54 @@ const Manifest: React.FC<{
 }> = ({
   manifest
 }) => (
-  manifest && (<link rel="manifest" href={
-      (typeof manifest === 'string') ? (manifest as string) : (manifest.href)
-  }/>)
+  manifest && (<link rel="manifest" href={getURLasString(manifest)}/>)
 )
+
+const getOGImageURL = (img: OGImage | undefined): string | null => {
+
+  if (!img) {
+    return null
+  }
+  if (typeof img === 'object' && 'url' in img) { // this is a OGImageDescriptor
+    return getURLasString(img.url)
+  }
+  return getURLasString(img) // this is a URL or string
+}
+
+const getTwitterImageURL = (img: TwitterImage | undefined): string | null => {
+
+  if (!img) {
+    return null
+  }
+  if (typeof img === 'object' && 'url' in img) { // this is a TwitterImageDescriptor
+    return getURLasString(img.url)
+  }
+  return getURLasString(img) // this is a URL or string
+}
+
+const OpenGraphComponent: React.FC<{
+  og: OpenGraph | undefined | null
+}> = ({
+  og 
+}) => (og && (<>
+  {og.url && (<meta property="og:url" content={(typeof og.url === 'string') ? (og.url as string) : (og.url.href)} />)}
+  {(og as any).type && (<meta property="og:type" content={(og as any).type} />)}
+  {og.title && (<meta property="og:title" content={getTitleFromTemplateString(og.title)!} />)}
+  {og.description && (<meta property="og:description" content={og.description} />)}
+  {og.images && (<meta property="og:image" content={getOGImageURL(Array.isArray(og.images) ? og.images[0] : og.images)!} />)}
+</>))
+
+export const TwitterComponent: React.FC<{
+  tw: Twitter | undefined | null
+}> = ({
+  tw 
+}) => (tw && (<>
+  {(tw as any).card && (<meta name="twitter:card" content={(tw as any).card} />)}
+  {tw.title && (<meta name="twitter:title" content={getTitleFromTemplateString(tw.title)!} />)}
+  {tw.description && (<meta name="twitter:description" content={tw.description} />)}
+  {tw.images && (<meta name="twitter:image" content={getTwitterImageURL(Array.isArray(tw.images) ? tw.images[0] : tw.images)!} />)}
+  {/* TO DO twitter:site or twitter:site:id as per https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */ }
+</>))
 
 
   // For use with pages router only.
@@ -130,9 +173,8 @@ const HeadMetadataComponent: React.FC<{
   metadata: Metadata
 }> = ({
   metadata
-}) => {
-  return <>
-    <Title title={metadata.title}/>
+}) => (<>
+
     {metadata.description && (      
       <meta name="description" content={metadata.description} />
     )}
@@ -145,7 +187,9 @@ const HeadMetadataComponent: React.FC<{
       {/* Icons: We only support this format for now */}
     <Icons icons={metadata.icons as IconDescriptor[]} />
     <Manifest manifest={metadata.manifest} />
+    <OpenGraphComponent og={metadata.openGraph} />
+    <TwitterComponent tw={metadata.twitter} />
   </>
-}
+)
 
 export default HeadMetadataComponent
