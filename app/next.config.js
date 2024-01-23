1827362fbde9ee1664708e3eb7434cdb69677e80
artemis prime <artemisprimedev@gmail.com>
2024-01-23 08:22:05 -0800

    next build

--- app/next.config.js
+++ app/next.config.js
@@ -41,7 +41,7 @@ module.exports = (phase, { defaultConfig }) => {
     },
     productionBrowserSourceMaps: true,
       // https://stackoverflow.com/questions/72621835/how-to-fix-you-may-need-an-appropriate-loader-to-handle-this-file-type-current
-    //transpilePackages: ['@luxdefi/ui'],
+    transpilePackages: ['@luxdefi/ui'],
 
     experimental: { esmExternals: 'loose' },
   }
