f66fc1114c278b1d432615cc8da9bfb3f9f79361
Zach Kelling <z@zeekay.io>
2024-01-23 17:20:47 +0100

    Enable eslint again

---
+++ app/.eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+      "prefer-const": "off",
+      "no-var": "off",
+      "no-extra-semi": "off",
+      "no-useless-escape": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/ban-types": "off",
+      "react/display-name": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/no-unknown-property": "off",
+      "react/jsx-no-target-blank": "off",
+      "react/no-unescaped-entities": "off",
+      "no-extra-boolean-cast": "off",
+      "no-case-declarations": "off",
+      "no-constant-condition": "off",
+    }
+}
