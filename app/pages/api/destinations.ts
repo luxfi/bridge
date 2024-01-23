4f2a15f019fc1dcfcc8bfa8ad9acd47d17de0191
Zach Kelling <z@zeekay.io>
2024-01-23 16:11:41 +0100

    Various fixes

--- app/pages/api/destinations.ts
+++ app/pages/api/destinations.ts
@@ -1,13 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { mainnetDestinations, testnetDestinations } from '../../destinations'
+import { mainnetSettings, testnetSettings } from '../../settings'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<typeof mainnetDestinations | typeof testnetDestinations>
+  res: NextApiResponse<typeof mainnetSettings | typeof testnetSettings>
 ) {
   const versionFromQuery = req.query.version as string;
   const isMainnet = versionFromQuery === 'mainnet' || process.env.NEXT_PUBLIC_API_VERSION === 'mainnet';
-  const destinations = isMainnet ? mainnetDestinations : testnetDestinations;
+  const settings = isMainnet ? mainnetSettings : testnetSettings;
 
-  res.status(200).json(destinations);
+  res.status(200).json(settings);
 }
--- app/pages/api/sources.ts
+++ app/pages/api/sources.ts
@@ -1,13 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { mainnetSources, testnetSources } from '../../sources'
+import { mainnetSettings, testnetSettings } from '../../settings'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<typeof mainnetSources | typeof testnetSources>
+  res: NextApiResponse<typeof mainnetSettings | typeof testnetSettings>
 ) {
   const versionFromQuery = req.query.version as string;
   const isMainnet = versionFromQuery === 'mainnet' || process.env.NEXT_PUBLIC_API_VERSION === 'mainnet';
-  const sources = isMainnet ? mainnetSources : testnetSources;
+  const settings = isMainnet ? mainnetSettings : testnetSettings;
 
-  res.status(200).json(sources);
+  res.status(200).json(settings);
 }
