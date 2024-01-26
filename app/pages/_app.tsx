b759709dfce29398c58d71ee72e1fadc9436bebe
artemis prime <artemisprimedev@gmail.com>
2024-01-25 23:41:32 -0800

    Using new font API from @luxdefi/ui

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { IntercomProvider } from 'react-use-intercom';
 import { SWRConfig } from 'swr'
 
-import { inter, drukTextWide } from '@luxdefi/ui/next-fonts'
+import PagesRouterFontVars from '@luxdefi/ui/next/pages-router-font-vars'
 
 import '../styles/globals.css'
 import '../styles/dialog-transition.css'
@@ -11,19 +11,12 @@ const INTERCOM_APP_ID = 'o1kmvctg'
 import "@rainbow-me/rainbowkit/styles.css";
 
 function App({ Component, pageProps }) {
+
   const router = useRouter()
+
   return (<>
-    <style jsx global>{`
-        html {
-          --font-inter: ${inter.style.fontFamily};
-          --font-druk-text-wide: ${drukTextWide.style.fontFamily};
-        }
-      `}</style>
-    <SWRConfig
-      value={{
-        revalidateOnFocus: false,
-      }}
-    >
+    <PagesRouterFontVars />
+    <SWRConfig value={{ revalidateOnFocus: false}}>
       <IntercomProvider appId={INTERCOM_APP_ID} initializeDelay={2500}>
         <Component key={router.asPath} {...pageProps} />
       </IntercomProvider>
