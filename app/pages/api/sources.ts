5514a773a18cbb0f4ce0ab272182846cb6acb82a
venuswhispers <venuswhisperer412@gmail.com>
2024-07-12 00:53:21 +0800

    api: fix

--- app/pages/api/sources.ts
+++ app/pages/api/sources.ts
@@ -4,13 +4,15 @@ import { mainnetSettings, testnetSettings } from '../../settings'
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{
-    network: string;
-    asset: string;
-  }[]>
+    data: {
+      network: string;
+      asset: string;
+    }[]
+  }>
 ) {
   const versionFromQuery = req.query.version as string;
   const isMainnet = versionFromQuery === 'mainnet' || process.env.NEXT_PUBLIC_API_VERSION === 'mainnet';
   const settings = isMainnet ? mainnetSettings : testnetSettings;
 
-  res.status(200).json(settings.sources.data);
+  res.status(200).json({ data: settings.sources.data });
 }
