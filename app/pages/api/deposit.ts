d806fb71dc9aeb27af057e562ed795cf04d78bf8
Jason <373422363@qq.com>
2024-07-15 19:52:38 +0800

    update:add prisma and update swapUserInfo model update deposit method

--- app/pages/api/deposit.ts
+++ app/pages/api/deposit.ts
@@ -1,6 +1,71 @@
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const depositAddress = "lux_wallet_address_example";
-  res.status(200).json({ address: depositAddress });
+import Joi from "joi";
+
+// 初始化 Prisma 客户端
+const prisma = new PrismaClient();
+
+// 定义请求体的校验模式
+const swapTransactionSchema = Joi.object({
+  amount: Joi.number().required(),
+  destination_address: Joi.string().required(),
+  destination_network: Joi.string().required(),
+  destination_token: Joi.string().required(),
+
+  source_address: Joi.string().required(),
+  source_network: Joi.string().required(),
+  source_token: Joi.string().required(),
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { error, value } = swapTransactionSchema.validate(req.body);
+
+    if (error) {
+      return res
+        .status(400)
+        .json({ error: "Invalid request data", details: error.details });
+    }
+    const {
+      amount,
+      destination_address,
+      destination_network,
+      destination_token,
+      refuel,
+      source_address,
+      source_network,
+      source_token,
+      use_deposit_address,
+    } = req.body;
+
+    try {
+      const newSwapTransaction = await prisma.swapUserInfo.create({
+        data: {
+          amount,
+          destinationAddress: destination_address,
+          destinationNetwork: destination_network,
+          destinationToken: destination_token,
+          refuel,
+          sourceAddress: source_address,
+          sourceNetwork: source_network,
+          sourceToken: source_token,
+          useDepositAddress: use_deposit_address,
+        },
+      });
+
+      res.status(200).json({ id: newSwapTransaction.id, message: "Success" });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "Error adding swap transaction to the database" });
+    }
+  } else {
+    // 如果请求方法不是 POST，返回 405 方法不允许
+    res.status(405).json({ error: "Method not allowed" });
+  }
 }
