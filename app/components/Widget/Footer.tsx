c28a571707f1540acd1d3be4716f2a1ff22fb49c
venuswhispers <venuswhisperer412@gmail.com>
2024-07-18 18:15:00 +0800

    update: currencies api update & network selection update

--- app/components/Widget/Footer.tsx
+++ app/components/Widget/Footer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react"
-import ReactPortal from "../Common/ReactPortal";
+import ReactPortal from "../../../Common/ReactPortal";
 
 
 const variants = {
