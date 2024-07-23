12a4353e1a228073d1a460e6eb93829ad96bf481
venuswhispers <venuswhisperer412@gmail.com>
2024-07-24 04:38:47 +0800

    chore: fix issue

--- app/components/Swap/Withdraw/Coinbase/Authorize.tsx
+++ app/components/Swap/Withdraw/Coinbase/Authorize.tsx
@@ -41,7 +41,7 @@ const Authorize: FC<Props> = ({ onAuthorized, stickyFooter, onDoNotConnect, hide
 
     const carouselRef = useRef<CarouselRef | null>(null)
     const exchange_internal_name = swap?.source_exchange
-    const asset_name = swap?.source_network_asset
+    const asset_name = swap?.source_asset;
 
     const exchange = layers.find(e => e.internal_name?.toLowerCase() === exchange_internal_name?.toLowerCase()) as Layer
     const currency = exchange?.assets.find(c => asset_name?.toLocaleUpperCase() === c.asset?.toLocaleUpperCase())
