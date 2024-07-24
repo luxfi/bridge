3faf95c9f94cdf4b586fd2982b328f1ed50e73a0
venuswhispers <venuswhisperer412@gmail.com>
2024-07-24 15:51:35 +0800

    udpate: token price api added

--- app/pages/api/tokens/price/[token_id].ts
+++ app/pages/api/tokens/price/[token_id].ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getTokenPrice } from "../../tokenAction";
 
+/**
+ * get token price
+ * @param req 
+ * @param res 
+ * /api/tokens/price/ETH
+ */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -18,18 +24,4 @@ export default async function handler(
         price
     }
   });
-
-//   if (req.method === "GET") {
-//     try {
-//       const result = await handlerGetSwap(swap_id as string);
-//       console.log("swap get result:", { result });
-
-//       res.status(200).json({ data: result });
-//     } catch (error) {
-//       res.status(500).json({ error: error.message });
-//     }
-//   } else {
-//     res.setHeader("Allow", ["GET"]);
-//     res.status(405).end(`Method ${req.method} Not Allowed`);
-//   }
 }
