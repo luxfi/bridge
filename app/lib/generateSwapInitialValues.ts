7bd9a284d89f0d668d699eecf3c0fa5ffed9e0c0
venuswhispers <venuswhisperer412@gmail.com>
2024-07-25 16:53:11 +0800

    update: networks initialization problems when creating swaps

--- app/lib/generateSwapInitialValues.ts
+++ app/lib/generateSwapInitialValues.ts
@@ -4,6 +4,19 @@ import { isValidAddress } from "./addressValidator";
 import { FilterDestinationLayers, FilterSourceLayers } from "../helpers/settingsHelper";
 import { BridgeAppSettings } from "../Models/BridgeAppSettings";
 import { SwapItem } from "./BridgeApiClient";
+import { Layer } from "../Models/Layer";
+import { Exchange } from "../Models/Exchange";
+import { NetworkCurrency } from "../Models/CryptoNetwork";
+
+const getExchangeAsset = (layers: Layer[], exchange?: Exchange, asset?: string) : NetworkCurrency | undefined => {
+    if (!exchange || !asset) {
+        return undefined;
+    } else {
+        const currency = exchange?.currencies?.find(c => c.asset === asset);
+        const layer = layers.find(n => n.internal_name === currency?.network);
+        return layer?.assets?.find(a => a?.asset === asset)
+    }
+}
 
 export function generateSwapInitialValues(settings: BridgeAppSettings, queryParams: QueryParams): SwapFormValues {
     const { destAddress, amount, asset, from, to, lockAsset } = queryParams
@@ -44,36 +57,34 @@ export function generateSwapInitialValues(settings: BridgeAppSettings, queryPara
 
 export function generateSwapInitialValuesFromSwap(swap: SwapItem, settings: BridgeAppSettings): SwapFormValues {
     const {
-        destination_address,
         requested_amount,
-        source_asset,
-        destination_asset,
         source_network,
-        destination_network,
         source_exchange,
+        source_asset,
+        destination_network,
         destination_exchange,
+        destination_asset,
+        destination_address,
         refuel
     } = swap
 
-    const { layers } = settings || {}
+    const { layers, exchanges } = settings || {}
 
-    const from = source_exchange ?
-        layers.find(l => l.internal_name === source_exchange)
-        : layers.find(l => l.internal_name === source_network)
-
-    const to = destination_exchange ?
-        layers.find(l => l.internal_name === destination_exchange)
-        : layers.find(l => l.internal_name === destination_network)
-
-    const fromCurrency = from?.assets.find(c => c.asset === source_asset)
-    const toCurrency = to?.assets.find(c => c.asset === destination_asset)
+    const from = layers.find(n => n.internal_name === source_network);
+    const fromExchange = exchanges.find(e => e.internal_name === source_exchange);
+    const fromCurrency = from ? from?.assets?.find(currency => currency?.asset === source_asset) : getExchangeAsset (layers, fromExchange, source_asset)
+    const to = layers?.find(l => l.internal_name === destination_network);
+    const toExchange = exchanges?.find(l => l.internal_name === destination_exchange);
+    const toCurrency = to ? to?.assets?.find(currency => currency?.asset === destination_asset) : getExchangeAsset (layers, fromExchange, destination_asset)
 
     const result: SwapFormValues = {
         from,
-        to,
-        amount: requested_amount?.toString(),
+        fromExchange,
         fromCurrency,
+        to,
+        toExchange,
         toCurrency,
+        amount: requested_amount?.toString(),
         destination_address,
         refuel: refuel
     }
