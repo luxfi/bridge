bbb102599ec7f4d558ebfd1e070deb60f013638a
venuswhispers <venuswhisperer412@gmail.com>
2024-07-31 06:34:31 +0800

    update: swap details display

--- app/pages/api/sentry-example-api.js
+++
@@ -1,6 +0,0 @@
-// A faulty API route to test Sentry's error monitoring
-export default function handler(req, res) {
-  console.log(req, res)
-  throw new Error("Sentry Example API Route Error");
-  // res.status(200).json({ name: "John Doe" });
-}
