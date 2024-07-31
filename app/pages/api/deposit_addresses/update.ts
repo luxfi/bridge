1a2cd3a807a7b4a74382b6e37b47ac867ef292c2
venuswhispers <venuswhisperer412@gmail.com>
2024-07-31 18:13:40 +0800

    udpate: deposit address

--- app/pages/api/deposit_addresses/update.ts
+++ app/pages/api/deposit_addresses/update.ts
@@ -15,12 +15,22 @@ export default async function handler(
     res: NextApiResponse
 ) {
     await prisma.depositAddress.deleteMany();
-    await Promise.all(Object.entries(deposit_addresses).map(([key, addresses]) => prisma.depositAddress.createMany({
-        data: addresses.map((a: string) => ({
-            address: a,
-            type: key
-        }))
-    })))
+    // await Promise.all(Object.entries(deposit_addresses).map(([key, addresses]) => prisma.depositAddress.createMany({
+    //     data: addresses.map((a: string) => ({
+    //         address: a,
+    //         type: key
+    //     }))
+    // })));
+    const data = Object.entries(deposit_addresses);
+    for (let index = 0; index < data.length; index++) {
+        const [key, addresses] = data[index];
+        await prisma.depositAddress.createMany({
+            data: addresses.map((a: string) => ({
+                address: a,
+                type: key
+            }))
+        })
+    }
     res.status(200).json({ status: 'success' });
 }
 
