87226cda8cc57a946af410d829bb535264e828ab
venuswhispers <venuswhisperer412@gmail.com>
2024-07-31 23:57:33 +0800

    feat: node urls added

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,11 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../lib/db";
+import Web3 from 'web3';
+
+/**
+ * generate random string
+ * @returns STRING
+ */
 export const generateRandomString = (): string => {
     const characters =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -6,4 +14,23 @@ export const generateRandomString = (): string => {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
+}
+
+/**
+ * get current block number
+ * @param network 
+ */
+export const getCurrentBlockNumber = async (network_name: string) => {
+    try {
+        const _network = await prisma.network.findUnique({
+            where: {
+                internal_name: network_name
+            }
+        });
+        if (_network?.type === 'evm') {
+            
+        }
+    } catch (err) {
+        return Promise.reject("err to fetch block number")
+    }
 }
