6fb57de5e085a80df7c0e56ef5fa5b6da14ca494
venuswhispers <venuswhisperer412@gmail.com>
2024-08-01 16:41:01 +0800

    update: deposit address & block number

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -20,11 +20,14 @@ export const generateRandomString = (): string => {
 const _getWeb3 = async (rpcList: string[]) => {
     for (let i = 0; i < rpcList.length; i++) {
         const rpcUrl = rpcList[i];
+        console.log(rpcUrl)
         const web3 = new Web3(new HttpProvider(String(rpcUrl)));
         try {
             await web3.eth.net.isListening();
             return Promise.resolve(web3);
-        } catch (err) {}
+        } catch (err) {
+            console.log('next rpc...')
+        }
     }
     return Promise.reject("cannot connect");
 };
@@ -44,7 +47,7 @@ export const getCurrentBlockNumber = async (network_name: string) => {
             }
         });
         if (_network?.type === 'evm') {
-            const web3 = await _getWeb3 (_network.nodes.map(n => n.url)).catch (err => {
+            const web3 = await _getWeb3(_network.nodes.map(n => n.url)).catch(err => {
                 throw "cannot connect to rpc..."
             });
             const blockNumber = await web3.eth.getBlockNumber();
@@ -55,15 +58,31 @@ export const getCurrentBlockNumber = async (network_name: string) => {
         return Promise.reject(0)
     }
 }
-
+/**
+ * get available deposit address
+ * @param network 
+ * @param asset 
+ * @returns 
+ */
 export const getAvailableDepositAddress = async (network: string, asset: string) => {
-    const data = await prisma.swap.findMany({
-        where: {
-            source_network: network,
-            source_asset: asset
-        },
-        select: {
-
+    try {
+        const _network = await prisma.network.findUnique ({
+            where: {
+                internal_name: network
+            }
+        });
+        const data: any = await prisma.$queryRaw`
+            SELECT id FROM DepositAddress WHERE type=${_network?.type} and id NOT IN (
+                SELECT deposit_address_id as id FROM "Swap"
+                WHERE source_network=${network} AND source_asset=${asset} AND status='user_transfer_pending'
+            )
+        `
+        if (data[0]) {
+            return Promise.resolve(data[0].id);
+        } else {
+            throw "no free deposit address";
         }
-    });
+    } catch (err) {
+        return Promise.reject("No free deposit address for this asset")
+    }
 }
