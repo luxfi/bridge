067fe7b42a0beb24504621739d26a94df53588dd
venuswhispers <venuswhisperer412@gmail.com>
2024-08-03 00:12:43 +0800

    update: deposit time expiry

--- app/pages/api/cron.ts
+++ app/pages/api/cron.ts
@@ -1,24 +1,47 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import Web3 from "web3";
 import prisma from "../../lib/db";
 
-import Web3 from "web3";
-import { mainnet } from "viem/chains";
 import { SwapStatus } from "@/Models/SwapStatus";
-import { swap } from "formik";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Swap, Network, Currency } from '@/Models/Swap';
+
+const checkSwap = async (swap: Swap) => {
+  const delay = Date.now() - new Date(swap.created_date).getTime();
+  if (delay > 15 * 60 * 1000) { // if there is no deposit for 15 mins
+    console.log("expired =======>", swap.id)
+    await prisma.swap.update({
+      where: {
+        id: swap.id
+      },
+      data: {
+        status: SwapStatus.Expired
+      }
+    })
+  } else {
+    
+  }
+}
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  
+
   const swaps = await prisma.swap.findMany({
     where: {
       status: SwapStatus.UserTransferPending
+    },
+    include: {
+      source_network: true,
+      source_asset: true,
+      destination_network: true,
+      destination_asset: true,
+      deposit_address: true,
     }
   });
 
-  console.log(swaps)
+  await Promise.all(swaps.map(checkSwap))
 
   return response.json({ 'status': "success" });
- 
+
 }
