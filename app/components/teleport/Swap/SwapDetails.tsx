a6a26db58ede3ad70b5f93cbda19b5ed10274563
venuswhispers <venuswhisperer412@gmail.com>
2024-09-03 03:23:11 +0800

    update: mpc node for db

--- app/components/teleport/Swap/SwapDetails.tsx
+++ app/components/teleport/Swap/SwapDetails.tsx
@@ -16,7 +16,9 @@ import {
     ethPriceAtom,
     swapStatusAtom,
     swapIdAtom,
-    bridgeTransferTransactionAtom
+    bridgeTransferTransactionAtom,
+    mpcSignatureAtom,
+    bridgeMintTransactionAtom
 } from '@/store/teleport'
 import { truncateDecimals } from '@/components/utils/RoundDecimals';
 import { ArrowRight, Router } from 'lucide-react';
@@ -34,6 +36,7 @@ import { useNetwork } from 'wagmi';
 import { parseUnits } from '@/lib/resolveChain';
 import axios from 'axios';
 import useWallet from '@/hooks/useWallet';
+import { getAddress } from 'ethers/lib/utils';
 
 interface IProps {
     className?: string
@@ -47,6 +50,7 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
     const [isMpcSigning, setIsMpcSigning] = React.useState<boolean>(false);
     //atoms
     const [bridgeTransferTransactionHash, setBridgeTransferTransactionHash] = useAtom(bridgeTransferTransactionAtom);
+    const [bridgeMintTransactionHash, setBridgeMintTransactionHash] = useAtom(bridgeMintTransactionAtom)
     const [sourceNetwork] = useAtom(sourceNetworkAtom);
     const [sourceAsset] = useAtom(sourceAssetAtom);
     const [destinationNetwork] = useAtom(destinationNetworkAtom);
@@ -56,10 +60,10 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
     const [swapStatus, setSwapStatus] = useAtom(swapStatusAtom);
     const [ethPrice] = useAtom(ethPriceAtom);
     const [swapId] = useAtom(swapIdAtom);
+    const [mpcSignature] = useAtom(mpcSignatureAtom);
     //token price
     const tokenPrice = sourceAsset?.asset === 'ETH' ? ethPrice : 1.0;
     //hooks
-    const router = useRouter();
     const signer = useEthersSigner({ chainId: sourceNetwork?.chain_id ?? 1 });
     const network = useNetwork();
     const { switchNetworkAsync } = useSwitchNetwork();
@@ -149,6 +153,7 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
      * @returns 
      */
     const getMpcSignature = async () => {
+
         setIsMpcSigning(true);
         if (!signer) {
             // toast.error(`No connected wallet. Please connect your wallet`);
@@ -176,18 +181,25 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
                 }
             });
             const data = await response.json();
-            console.log({ data })
 
             console.log({
                 amt_: sourceAmount,
                 hashedId_: Web3.utils.keccak256(bridgeTransferTransactionHash),
                 toTargetAddrStr_: destinationAddress,
-                signedTXInfo_: data.signature,
+                signedTXInfo_: data.data.signature,
                 tokenAddrStr_: destinationAsset?.contract_address,
                 chainId_: sourceNetwork?.chain_id,
                 fromTokenDecimal_: sourceAsset?.decimals,
                 vault_: true
             });
+
+            await axios.post(`/api/swaps/mpcsign/${swapId}`, {
+                txHash: data.data.signature,
+                amount: sourceAmount,
+                from: signer?._address,
+                to: CONTRACTS[String(sourceNetwork?.chain_id)].teleporter
+            });
+            setSwapStatus("user_withdraw_pending");
         } catch (err) {
             console.log("mpc sign request failed:::", err)
         } finally {
@@ -195,9 +207,83 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
         }
     }
 
+    const withdrawToken = async () => {
+        const data = {
+            amt_: sourceAmount,
+            hashedId_: Web3.utils.keccak256(bridgeTransferTransactionHash),
+            toTargetAddrStr_: destinationAddress,
+            signedTXInfo_: mpcSignature,
+            tokenAddrStr_: destinationAsset?.contract_address,
+            chainId_: sourceNetwork?.chain_id,
+            fromTokenDecimal_: sourceAsset?.decimals,
+            vault_: true
+        }
+
+        console.log(data)
+
+        if (!signer) {
+            // toast.error(`No connected wallet. Please connect your wallet`);
+            connectWallet('evm')
+            return;
+        }
+        if (!sourceNetwork || !sourceAsset || !destinationNetwork || !destinationAsset || !switchNetworkAsync) {
+            return;
+        }
+        try {
+            setIsTokenTransferring(true);
+            // if (network?.chain?.id !== destinationNetwork.chain_id) {
+            if (network?.chain?.id !== 11155111) {
+                setBridgeTranferNotice(`Switching to ${sourceNetwork.display_name}...`);
+                await switchNetworkAsync(11155111)
+                // await switchNetworkAsync(destinationNetwork.chain_id)
+            }
+            const _amount = parseUnits(String(sourceAmount), sourceAsset.decimals);
+
+
+            setBridgeTranferNotice(`Minting ${destinationAsset.asset}...`);
+            const bridgeContract = new Contract(
+                CONTRACTS[11155111].teleporter,
+                // CONTRACTS[destinationNetwork.chain_id].teleporter,
+                teleporterABI,
+                signer,
+            )
+
+            const _bridgeMintTx = await bridgeContract.bridgeMintStealth(
+                _amount,
+                data.hashedId_,
+                data.toTargetAddrStr_,
+                data.signedTXInfo_,
+                data.tokenAddrStr_,
+                data.chainId_,
+                data.fromTokenDecimal_,
+                String(data.vault_)
+            );
+            await _bridgeMintTx.wait();
+            await axios.post(`/api/swaps/success/${swapId}`, {
+                txHash: _bridgeMintTx.hash,
+                amount: sourceAmount,
+                from: signer?._address,
+                to: CONTRACTS[sourceNetwork.chain_id].teleporter
+            });
+            setBridgeMintTransactionHash(_bridgeMintTx.hash);
+            setSwapStatus("success");
+        } catch (err) {
+            console.log(err)
+            if (String(err).includes("user rejected transaction")) {
+                toast.error(`User rejected transaction`)
+            } else {
+                toast.error(`Failed to run transaction`)
+            }
+        } finally {
+            setIsTokenTransferring(false);
+        }
+    }
+
     React.useEffect(() => {
         if (swapStatus === "teleport_processing_pending") {
-            // getMpcSignature();
+            getMpcSignature();
+        } else if (swapStatus === "user_withdraw_pending") {
+            withdrawToken();
         }
     }, [swapStatus]);
 
@@ -318,7 +404,7 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
                                         <Gauge value={100} size="verySmall" showCheckmark={true} />
                                     </span>
                                     <div className='flex flex-col items-center text-sm'>
-                                        <span>Token transferred</span>
+                                        <span>{sourceAsset?.asset} transferred</span>
                                         <div className='underline flex gap-2 items-center'>
                                             {shortenAddress(bridgeTransferTransactionHash)}
                                             <Tooltip>
@@ -369,7 +455,7 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
                 </div>
             </div>
         )
-    } else if (swapStatus === 'Withdraw') {
+    } else if (swapStatus === 'user_withdraw_pending') {
         return (
             <div className={`w-full flex flex-col ${className}`}>
                 <div className='space-y-5'>
@@ -383,9 +469,33 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
                                     <Gauge value={60} size="medium" />
                                 </span>
                                 <div className='mt-2'>Withdraw Your LETH</div>
-                                <div className='text-sm !mt-2'>Estimated processing time for confirmation: ~15s</div>
                             </div>
-                            <div className='flex py-5'>
+                            <div className='flex flex-col gap-2 py-5'>
+                                <div className='flex gap-3 items-center'>
+                                    <span className="">
+                                        <Gauge value={100} size="verySmall" showCheckmark={true} />
+                                    </span>
+                                    <div className='flex flex-col items-center text-sm'>
+                                        <span>{sourceAsset?.asset} transferred</span>
+                                        <div className='underline flex gap-2 items-center'>
+                                            {shortenAddress(bridgeTransferTransactionHash)}
+                                            <Tooltip>
+                                                <TooltipTrigger asChild>
+                                                    <a
+                                                        target={"_blank"}
+                                                        href={sourceNetwork?.transaction_explorer_template?.replace("{0}", bridgeTransferTransactionHash)}
+                                                        className='cursor-pointer'
+                                                    >
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" /><path d="m21 3-9 9" /><path d="M15 3h6v6" /></svg>
+                                                    </a>
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>View Transaction</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div className='flex gap-3 items-center'>
                                     <span className="">
                                         <Gauge value={100} size="verySmall" showCheckmark={true} />
@@ -395,7 +505,7 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
                                     </div>
                                 </div>
                             </div>
-                            <button onClick={() => setSwapStatus("Success")} className="border border-muted-3 disabled:border-[#404040] items-center space-x-1 disabled:opacity-80 disabled:cursor-not-allowed relative w-full flex justify-center font-semibold rounded-md transform transition duration-200 ease-in-out hover:bg-primary-hover bg-primary-lux text-primary-fg disabled:hover:bg-primary-lux py-3 px-2 md:px-3 plausible-event-name=Swap+initiated">
+                            <button onClick={withdrawToken} className="border border-muted-3 disabled:border-[#404040] items-center space-x-1 disabled:opacity-80 disabled:cursor-not-allowed relative w-full flex justify-center font-semibold rounded-md transform transition duration-200 ease-in-out hover:bg-primary-hover bg-primary-lux text-primary-fg disabled:hover:bg-primary-lux py-3 px-2 md:px-3 plausible-event-name=Swap+initiated">
                                 Withdraw Your {destinationAsset?.asset}
                             </button>
                         </div>
@@ -403,7 +513,7 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
                 </div>
             </div>
         )
-    } else if (swapStatus === 'Success') {
+    } else if (swapStatus === 'success') {
         return (
             <div className={`w-full flex flex-col ${className}`}>
                 <div className='space-y-5'>
@@ -436,12 +546,16 @@ const SwapDetails: React.FC<IProps> = ({ className }) => {
                                     <div className='flex flex-col text-sm'>
                                         <span>Your {destinationAsset?.asset} has been arrived</span>
                                         <div className='underline flex gap-2 items-center'>
-                                            {shortenAddress("0xa684c5721e54B871111CE1F1E206d669a7e7F0a5")}
+                                            {shortenAddress(bridgeMintTransactionHash)}
                                             <Tooltip>
                                                 <TooltipTrigger asChild>
-                                                    <span className='cursor-pointer'>
+                                                    <a
+                                                        target={"_blank"}
+                                                        href={destinationNetwork?.transaction_explorer_template?.replace("{0}", bridgeMintTransactionHash)}
+                                                        className='cursor-pointer'
+                                                    >
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-arrow-out-up-right"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" /><path d="m21 3-9 9" /><path d="M15 3h6v6" /></svg>
-                                                    </span>
+                                                    </a>
                                                 </TooltipTrigger>
                                                 <TooltipContent>
                                                     <p>View Transaction</p>
