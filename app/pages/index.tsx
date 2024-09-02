ee6ec4e063de3f16bee675c6e6ce2afa0eb506f4
venuswhispers <venuswhisperer412@gmail.com>
2024-09-03 07:06:18 +0800

    chore: code refactoring

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,10 @@
+
 import { InferGetServerSidePropsType } from 'next'
 
 import Layout from '../components/layout'
-import Swap from '../components/swapComponent'
-import Teleporter from '../components/Teleport'
-import { getServerSideProps } from '../helpers/getSettings'
+import Swap from '@/components/swapComponent'
+import Teleporter from '@/components/Teleport/index'
+import { getServerSideProps } from '@/helpers/getSettings'
 
 export default function Home({ settings, themeData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
