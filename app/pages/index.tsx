bf28ed7d5fbc362b87b4107f4aa0e7db4dc10600
venuswhispers <venuswhisperer412@gmail.com>
2024-09-03 08:30:30 +0800

    chore: code refactoring

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,13 @@
 
 import { InferGetServerSidePropsType } from 'next'
+import dynamic from 'next/dynamic';
 
 import Layout from '../components/layout'
 import Swap from '@/components/swapComponent'
-import Teleporter from '../components/Teleport/Swap/index'
+// import Teleporter from '../components/Teleport/Swap/index'
 import { getServerSideProps } from '@/helpers/getSettings'
+
+const Teleporter = dynamic(() => import('@/components/Teleport/Swap/index'));
 
 export default function Home({ settings, themeData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
