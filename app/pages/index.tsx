f28fc6c180df72c1a34d9a55c736b90675740433
venuswhispers <venuswhisperer412@gmail.com>
2024-09-03 11:33:17 +0800

    update: teleporter switcher added

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,17 +1,28 @@
 
-import { InferGetServerSidePropsType } from 'next'
-import dynamic from 'next/dynamic';
-
 import Layout from '../components/layout'
+import React from 'react';
 import Swap from '@/components/swapComponent'
-import Teleporter from '../components/teleport/swap/Teleporter'
+import Teleporter from '@/components/teleport/swap/Teleporter'
+import { InferGetServerSidePropsType } from 'next'
 import { getServerSideProps } from '@/helpers/getSettings'
+import { classNames } from '@/components/utils/classNames';
+import ToggleButton from '@/components/buttons/toggleButton';
 
 export default function Home({ settings, themeData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [checked, setChecked] = React.useState<boolean>(false);
   return (
     <Layout settings={settings} themeData={themeData}>
-      <Teleporter />
-      {/* <Swap /> */}
+      <div className='flex items-center gap-2 pb-3'>
+        <ToggleButton
+          value={checked}
+          onChange={(value: boolean) => setChecked(value)}
+          name="Teleport"
+        />
+        Teleport
+      </div>
+      {
+        checked ? <Teleporter /> : <Swap />
+      }
     </Layout>
   )
 }
