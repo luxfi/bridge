47f73078bdd1afc347948682c776b6697cdcf23c
venuswhispers <venuswhisperer412@gmail.com>
2024-09-03 09:35:59 +0800

    chore: code refactoring

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 
 import Layout from '../components/layout'
 import Swap from '@/components/swapComponent'
-import Teleporter from '../components/teleport/swap/index'
+import Teleporter from '../components/teleport/swap/Teleporter'
 import { getServerSideProps } from '@/helpers/getSettings'
 
 export default function Home({ settings, themeData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
