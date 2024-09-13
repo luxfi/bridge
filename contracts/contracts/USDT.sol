4a9a932d4f024d7d16d5d0f2026018339687fe39
venuswhispers <venuswhisperer412@gmail.com>
2024-09-13 16:06:47 +0800

    update: vault contracts

--- contracts/contracts/USDT.sol
+++ contracts/contracts/USDT.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract USDT is ERC20 {
+contract USDTToken is ERC20 {
     constructor() ERC20("USDT Coin", "USDT") {
         _mint(msg.sender, 10000000000 * 10 ** decimals());
     }
 
-    function decimals() public pure override returns (uint8) {
-        return 6; // Or whatever the constant value is
+    function decimals() public view override returns (uint8) {
+        return 6;
     }
 
     function mint(address account, uint256 amount) public {
