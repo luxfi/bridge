82cf3fcefcb2af09d6bb4364e42ab3e1a56fe164
venuswhispers <venuswhisperer412@gmail.com>
2024-09-25 03:53:06 +0800

    update: lux vault and eth vault

--- contracts/contracts/LERC4626.sol
+++ contracts/contracts/LERC4626.sol
@@ -7,7 +7,11 @@ pragma solidity ^0.8.20;
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract MyERC4626 is ERC4626{
-    constructor(IERC20 asset_, string memory name, string memory symbol) ERC4626(asset_) ERC20(name, symbol) {
-    }
+
+contract LERC4626 is ERC4626 {
+    constructor(
+        IERC20 asset_,
+        string memory name,
+        string memory symbol
+    ) ERC4626(asset_) ERC20(name, symbol) {}
 }
