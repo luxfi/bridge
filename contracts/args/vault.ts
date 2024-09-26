82fd8174c0034a5b73797ff8706bb99b8b364d65
venuswhispers <venuswhisperer412@gmail.com>
2024-09-26 15:40:59 +0800

    update: smart contracts

---
+++ contracts/args/vault.ts
@@ -0,0 +1,7 @@
+module.exports = [
+    [
+        "0xFa005D52398B4BdAAf73C4e2E3AF8095533D790E", //usdt
+        "0x42449554b0c7D85EbD488e14D7D48c6A78D3F9Be", //usdc
+        "0xc16ECFE3cB80e142d7110b97a442d4caAA203ABf", //dai
+    ]
+];
