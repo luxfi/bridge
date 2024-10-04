b774c33e35593a14785d11f3cdf11f49c8f7025f
venuswhispers <venuswhisperer412@gmail.com>
2024-10-04 14:24:19 +0800

    update: source and des pairs

--- apps/bridge/components/teleport/swap/from/NetworkSelectWrapper.tsx
+++ apps/bridge/components/teleport/swap/from/NetworkSelectWrapper.tsx
@@ -37,8 +37,6 @@ export default function NetworkSelectWrapper<T>({
         }
     }, [])
 
-    console.log(network)
-
     return (
         <>
             <div className="flex items-center relative">
