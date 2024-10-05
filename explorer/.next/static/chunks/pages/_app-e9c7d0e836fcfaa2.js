112739d12f4afb086c39d8d24729ff6b45c20ced
venuswhispers <venuswhisperer412@gmail.com>
2024-10-06 05:41:55 +0800

    Remove accidentally added .next folder and update .gitignore

--- explorer/.next/static/chunks/pages/_app-e9c7d0e836fcfaa2.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{4778:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(2926)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(4778),_(8589)}),_N_E=n.O()}]);
-//# sourceMappingURL=_app-e9c7d0e836fcfaa2.js.map
--- explorer/.next/static/chunks/pages/_error-e408d3b652f1a6a6.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{5984:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(1320)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=5984)}),_N_E=n.O()}]);
-//# sourceMappingURL=_error-e408d3b652f1a6a6.js.map
