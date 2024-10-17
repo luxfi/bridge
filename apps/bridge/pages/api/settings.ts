4e7b7dee7da4cff8cfa764129884ca6e580e0e87
venuswhispers <venuswhisperer412@gmail.com>
2024-10-17 18:52:42 -0400

    chore: code refactoring

--- apps/bridge/pages/api/settings.ts
+++ apps/bridge/pages/api/settings.ts
@@ -1,12 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { mainnetSettings, testnetSettings } from '../../settings'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { mainnetSettings, testnetSettings } from "../../settings";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<typeof mainnetSettings | typeof testnetSettings>
 ) {
   const versionFromQuery = req.query.version as string;
-  const isMainnet = versionFromQuery === 'mainnet' || process.env.NEXT_PUBLIC_API_VERSION === 'mainnet';
+  const isMainnet =
+    versionFromQuery === "mainnet" ||
+    process.env.NEXT_PUBLIC_API_VERSION === "mainnet";
   const settings = isMainnet ? mainnetSettings : testnetSettings;
 
   res.status(200).json(settings);
