6106d28cf9e963a5908552e337058730ca41f3ab
venuswhispers <venuswhisperer412@gmail.com>
2024-10-24 16:51:20 -0400

    update: eth mainnet added

--- contracts/scripts/bridge/deploy.ts
+++ contracts/scripts/bridge/deploy.ts
@@ -1,15 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-    /////Bridge
-    const _signer = await ethers.getContractFactory("Bridge");
-    const _bridge = await _signer.deploy();
-    console.log("Bridge address:", await _bridge.getAddress());
+  /////Bridge
+  const _signer = await ethers.getContractFactory("Bridge");
+  const _bridge = await _signer.deploy();
+  console.log("Bridge address:", await _bridge.getAddress());
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
