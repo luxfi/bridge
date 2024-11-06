9f4e14994592629a82a61debb97c687d75f5365e
venuswhispers <venuswhisperer412@gmail.com>
2024-11-06 18:57:41 -0500

    update: bridge server

--- apps/server/src/helpers/mpcHelper.ts
+++ apps/server/src/helpers/mpcHelper.ts
@@ -2,9 +2,12 @@ import axios from "axios"
 const mpc_nodes = [
   // "http://127.0.0.1:6000",
   // "http://127.0.0.1:6000",
-  "https://teleport-0.lux.network",
-  "https://teleport-1.lux.network"
+  // "https://teleport-0.lux.network",
+  // "https://teleport-1.lux.network"
   // 'https://teleport.lux.network/node2',
+  "http://mpc-node-0:6000",
+  "http://mpc-node-1:6000"
+  // "http://mpc-node-2:6000"
 ]
 
 export const getSigFromMpcOracleNetwork = (signData: { txId: string; fromNetworkId: string; toNetworkId: string; toTokenAddress: string; msgSignature: string; receiverAddressHash: string }) => {
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -17,6 +17,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use("/api/swaps", swaps)
 app.use("/api/explorer", explorer)
 
+app.get("/", async (req, res) => {
+  res.json(">>> Hello world. We are LUX!!!")
+})
+
 app.listen(port, "0.0.0.0", function () {
   console.log(`>> Server is Running At: Port ${port}`)
 })
