d7cf4dd265331ae1b2d8d699c5984006f090a338
artem ash <artemisprimedev@gmail.com>
2024-11-05 23:30:24 -0800

    builds

--- apps/bridge/src/app/api/settings/route.ts
+++ apps/bridge/src/app/api/settings/route.ts
@@ -1,15 +1,14 @@
-import type { NextRequest, NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { mainnetSettings, testnetSettings } from "@/settings";
 
-export default async function handler(
+export async function GET (
   req: NextRequest,
-  res: NextResponse<typeof mainnetSettings | typeof testnetSettings>
 ) {
   // const versionFromQuery = req.query.version as string;
   // const isMainnet =
   //   versionFromQuery === "mainnet" ||
   //   process.env.NEXT_PUBLIC_API_VERSION === "mainnet";
   // const settings = isMainnet ? mainnetSettings : testnetSettings;
-  
+
   return Response.json(mainnetSettings, {status: 200})
 }
