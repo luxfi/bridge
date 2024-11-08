16b13db23dbfd69fc394d06de63d38eb01abadd9
venuswhispers <venuswhisperer412@gmail.com>
2024-11-08 14:22:56 -0500

    remove: ton & starknet connect

--- apps/bridge/src/components/SwapHistory/TransfersWrapper.tsx
+++ apps/bridge/src/components/SwapHistory/TransfersWrapper.tsx
@@ -1,27 +1,30 @@
-'use client'
-import TransactionsHistory from './index'
-import { useAuthState, UserType } from '@/context/authContext'
-import { FormWizardProvider } from '@/context/formWizardProvider'
-import { TimerProvider } from '@/context/timerContext'
-import { AuthStep } from '@/Models/Wizard'
-import GuestCard from '../guestCard'
+"use client";
+import TransactionsHistory from "./index";
+import { useAuthState, UserType } from "@/context/authContext";
+import { FormWizardProvider } from "@/context/formWizardProvider";
+import { TimerProvider } from "@/context/timerContext";
+import { AuthStep } from "@/Models/Wizard";
+import GuestCard from "../guestCard";
 
 const TransfersWrapper: React.FC = () => {
-    const { userType } = useAuthState()
+  const { userType } = useAuthState();
 
-    return (
-        <div className=''>
-            <TransactionsHistory />
-            {
-                userType && userType != UserType.AuthenticatedUser &&
-                <FormWizardProvider initialStep={AuthStep.Email} initialLoading={false} hideMenu>
-                    <TimerProvider>
-                        <GuestCard />
-                    </TimerProvider>
-                </FormWizardProvider>
-            }
-        </div>
-    )
-}
+  return (
+    <div className="">
+      <TransactionsHistory />
+      {userType && userType != UserType.AuthenticatedUser && (
+        <FormWizardProvider
+          initialStep={AuthStep.Email}
+          initialLoading={false}
+          hideMenu
+        >
+          <TimerProvider>
+            <GuestCard />
+          </TimerProvider>
+        </FormWizardProvider>
+      )}
+    </div>
+  );
+};
 
-export default TransfersWrapper
+export default TransfersWrapper;
--- apps/bridge/src/components/buttons/connectButton.tsx
+++ apps/bridge/src/components/buttons/connectButton.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { type ReactNode, useState } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "../shadcn/popover";
@@ -7,11 +7,11 @@ import { NetworkType } from "../../Models/CryptoNetwork";
 import RainbowIcon from "../icons/Wallets/Rainbow";
 import TON from "../icons/Wallets/TON";
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "../shadcn/dialog";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 import MetaMaskIcon from "../icons/Wallets/MetaMask";
@@ -26,171 +26,169 @@ import Solflare from "../icons/Wallets/Solflare";
 import CoinbaseIcon from "../icons/Wallets/Coinbase";
 
 const ConnectButton = ({
-    children,
-    className,
-    onClose,
+  children,
+  className,
+  onClose,
 }: {
-    children: ReactNode;
-    className?: string;
-    onClose?: () => void;
+  children: ReactNode;
+  className?: string;
+  onClose?: () => void;
 }) => {
-    const { connectWallet, wallets } = useWallet();
-    const [open, setOpen] = useState<boolean>();
-    const { isMobile } = useWindowDimensions();
+  const { connectWallet, wallets } = useWallet();
+  const [open, setOpen] = useState<boolean>();
+  const { isMobile } = useWindowDimensions();
 
-    const knownConnectors = [
-        {
-            name: "EVM",
-            id: "evm",
-            type: NetworkType.EVM,
-        },
-        {
-            name: "Starknet",
-            id: "starknet",
-            type: NetworkType.Starknet,
-        },
-        {
-            name: "TON",
-            id: "ton",
-            type: NetworkType.TON,
-        },
-        {
-            name: "Solana",
-            id: "solana",
-            type: NetworkType.Solana,
-        },
-    ];
-    const filteredConnectors = knownConnectors.filter(
-        (c) => !wallets.map((w) => w?.providerName).includes(c.id)
-    );
-    return isMobile ? (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger aria-label="Connect wallet">{children}</DialogTrigger>
-            <DialogContent className="sm:max-w-[425px] text-muted bg-level-1 border-[#404040]">
-                <DialogHeader>
-                    <DialogTitle className="text-center">
-                        Link a new wallet
-                    </DialogTitle>
-                </DialogHeader>
-                <div className="space-y-2">
-                    {filteredConnectors.map((connector, index) => (
-                        <button
-                            type="button"
-                            key={index}
-                            className="w-full h-fit rounded py-2 px-3 hover:bg-level-2 "
-                            onClick={() => {
-                                connectWallet(connector.id);
-                                setOpen(false);
-                                onClose && onClose();
-                            }}
-                        >
-                            <div className="flex space-x-2 items-center">
-                                {connector && (
-                                    <div className="inline-flex items-center relative">
-                                        <ResolveConnectorIcon
-                                            connector={connector.id}
-                                            className="w-7 h-7 p-0.5 rounded-full border border-level-2"
-                                        />
-                                    </div>
-                                )}
-                                <p>{connector.name}</p>
-                            </div>
-                        </button>
-                    ))}
-                </div>
-            </DialogContent>
-        </Dialog>
-    ) : (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger
-                asChild={true}
-                aria-label="Connect wallet"
-                disabled={filteredConnectors.length == 0}
-                className={`${className} disabled:cursor-not-allowed `}
+  const knownConnectors = [
+    {
+      name: "EVM",
+      id: "evm",
+      type: NetworkType.EVM,
+    },
+    // {
+    //   name: "Starknet",
+    //   id: "starknet",
+    //   type: NetworkType.Starknet,
+    // },
+    // {
+    //   name: "TON",
+    //   id: "ton",
+    //   type: NetworkType.TON,
+    // },
+    {
+      name: "Solana",
+      id: "solana",
+      type: NetworkType.Solana,
+    },
+  ];
+  const filteredConnectors = knownConnectors.filter(
+    (c) => !wallets.map((w) => w?.providerName).includes(c.id)
+  );
+  return isMobile ? (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger aria-label="Connect wallet">{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] text-muted bg-level-1 border-[#404040]">
+        <DialogHeader>
+          <DialogTitle className="text-center">Link a new wallet</DialogTitle>
+        </DialogHeader>
+        <div className="space-y-2">
+          {filteredConnectors.map((connector, index) => (
+            <button
+              type="button"
+              key={index}
+              className="w-full h-fit rounded py-2 px-3 hover:bg-level-2 "
+              onClick={() => {
+                connectWallet(connector.id);
+                setOpen(false);
+                onClose && onClose();
+              }}
             >
-                {children}
-            </PopoverTrigger>
-            <PopoverContent className="flex flex-col items-start gap-2 w-fit bg-level-1 border-[#404040]">
-                {filteredConnectors.map((connector, index) => (
-                    <button
-                        type="button"
-                        key={index}
-                        className="w-full h-full hover:bg-level-2 rounded py-2 px-3"
-                        onClick={() => {
-                            connectWallet(connector.id);
-                            setOpen(false);
-                            onClose && onClose();
-                        }}
-                    >
-                        <div className="flex space-x-2 items-center">
-                            {connector && (
-                                <div className="inline-flex items-center relative">
-                                    <ResolveConnectorIcon
-                                        connector={connector.id}
-                                        className="w-7 h-7 p-0.5 rounded-full border border-level-2"
-                                    />
-                                </div>
-                            )}
-                            <p>{connector.name}</p>
-                        </div>
-                    </button>
-                ))}
-            </PopoverContent>
-        </Popover>
-    );
+              <div className="flex space-x-2 items-center">
+                {connector && (
+                  <div className="inline-flex items-center relative">
+                    <ResolveConnectorIcon
+                      connector={connector.id}
+                      className="w-7 h-7 p-0.5 rounded-full border border-level-2"
+                    />
+                  </div>
+                )}
+                <p>{connector.name}</p>
+              </div>
+            </button>
+          ))}
+        </div>
+      </DialogContent>
+    </Dialog>
+  ) : (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger
+        asChild={true}
+        aria-label="Connect wallet"
+        disabled={filteredConnectors.length == 0}
+        className={`${className} disabled:cursor-not-allowed `}
+      >
+        {children}
+      </PopoverTrigger>
+      <PopoverContent className="flex flex-col items-start gap-2 w-fit bg-level-1 border-[#404040]">
+        {filteredConnectors.map((connector, index) => (
+          <button
+            type="button"
+            key={index}
+            className="w-full h-full hover:bg-level-2 rounded py-2 px-3"
+            onClick={() => {
+              connectWallet(connector.id);
+              setOpen(false);
+              onClose && onClose();
+            }}
+          >
+            <div className="flex space-x-2 items-center">
+              {connector && (
+                <div className="inline-flex items-center relative">
+                  <ResolveConnectorIcon
+                    connector={connector.id}
+                    className="w-7 h-7 p-0.5 rounded-full border border-level-2"
+                  />
+                </div>
+              )}
+              <p>{connector.name}</p>
+            </div>
+          </button>
+        ))}
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default ConnectButton;
 
 const ResolveConnectorIcon = ({
-    connector,
-    className,
+  connector,
+  className,
 }: {
-    connector: string;
-    className: string;
+  connector: string;
+  className: string;
 }) => {
-    switch (connector.toLowerCase()) {
-        case KnownConnectors.EVM:
-            return (
-                <div className="-space-x-2 flex">
-                    <RainbowIcon className={className} />
-                    <MetaMaskIcon className={className} />
-                    <WalletConnectIcon className={className} />
-                </div>
-            );
-        case KnownConnectors.Starknet:
-            return (
-                <div className="-space-x-2 flex">
-                    <Braavos className={className} />
-                    <Argent className={className} />
-                    <ArgentX className={className} />
-                </div>
-            );
-        case KnownConnectors.TON:
-            return (
-                <div className="-space-x-2 flex">
-                    <TonKeeper className={className} />
-                    <OpenMask className={className} />
-                    <TON className={className} />
-                </div>
-            );
-        case KnownConnectors.Solana:
-            return (
-                <div className="-space-x-2 flex">
-                    <Phantom className={className} />
-                    <Solflare className={className} />
-                    <WalletConnectIcon className={className} />
-                    <CoinbaseIcon className={className} />
-                </div>
-            );
-        default:
-            return <></>;
-    }
+  switch (connector.toLowerCase()) {
+    case KnownConnectors.EVM:
+      return (
+        <div className="-space-x-2 flex">
+          <RainbowIcon className={className} />
+          <MetaMaskIcon className={className} />
+          <WalletConnectIcon className={className} />
+        </div>
+      );
+    // case KnownConnectors.Starknet:
+    //   return (
+    //     <div className="-space-x-2 flex">
+    //       <Braavos className={className} />
+    //       <Argent className={className} />
+    //       <ArgentX className={className} />
+    //     </div>
+    //   );
+    // case KnownConnectors.TON:
+    //     return (
+    //         <div className="-space-x-2 flex">
+    //             <TonKeeper className={className} />
+    //             <OpenMask className={className} />
+    //             <TON className={className} />
+    //         </div>
+    //     );
+    case KnownConnectors.Solana:
+      return (
+        <div className="-space-x-2 flex">
+          <Phantom className={className} />
+          <Solflare className={className} />
+          <WalletConnectIcon className={className} />
+          <CoinbaseIcon className={className} />
+        </div>
+      );
+    default:
+      return <></>;
+  }
 };
 
 const KnownConnectors = {
-    Starknet: "starknet",
-    EVM: "evm",
-    TON: "ton",
-    Solana: "solana",
+  Starknet: "starknet",
+  EVM: "evm",
+  TON: "ton",
+  Solana: "solana",
 };
