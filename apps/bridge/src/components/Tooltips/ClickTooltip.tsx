bcb691790c6841b69a1b938e05c8224b3cb2aea1
artem ash <artemisprimedev@gmail.com>
2024-11-09 12:07:31 -0800

    bridge now uses @hanzo/ui/primitives where possible   (no redundancies)

--- apps/bridge/src/components/Tooltips/ClickTooltip.tsx
+++ apps/bridge/src/components/Tooltips/ClickTooltip.tsx
@@ -1,21 +1,24 @@
-import { Info } from 'lucide-react';
-import { Popover, PopoverContent, PopoverTrigger } from '../shadcn/popover';
+import React, { type PropsWithChildren } from 'react'
+import { Info } from 'lucide-react'
+import { 
+  Popover, 
+  PopoverContent, 
+  PopoverTrigger,
+} from '@hanzo/ui/primitives'
 
-type Props = {
-    children?: JSX.Element | JSX.Element[],
-    text: string | JSX.Element | JSX.Element[];
-    moreClassNames?: string
-}
 
-const ClickTooltip: React.FC<Props> = (({ children, text, moreClassNames }) => {
-    return (
-        <Popover>
-            <PopoverTrigger>
-                <Info className="h-4 " aria-hidden="true" strokeWidth={2.5} />
-            </PopoverTrigger>
-            <PopoverContent className='text-sm'>{text}</PopoverContent>
-        </Popover>
-    )
-})
+const ClickTooltip: React.FC<{
+  text: string | React.ReactNode
+} & PropsWithChildren> = ({  
+  children,
+  text 
+}) => (
+  <Popover>
+    <PopoverTrigger>
+      <Info className="h-4 " aria-hidden="true" strokeWidth={2.5} />
+    </PopoverTrigger>
+    <PopoverContent className='text-sm'>{text}{children}</PopoverContent>
+  </Popover>
+)
 
 export default ClickTooltip
