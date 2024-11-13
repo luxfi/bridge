70bd736a6377711b11d2e0372e1e70a99463c20c
venuswhispers <venuswhisperer412@gmail.com>
2024-11-13 10:16:17 -0500

    update: remove api and moved to new server

--- apps/explorer/context/settings.tsx
+++ apps/explorer/context/settings.tsx
@@ -1,27 +1,28 @@
 'use client'
 
+import React, { ReactNode } from 'react'
+import useSWR from 'swr';
 import AppSettings from '@/lib/AppSettings';
 import { ApiResponse } from '@/models/ApiResponse';
 import { CryptoNetwork } from '@/models/CryptoNetwork';
 import { BridgeAppSettings } from '@/models/BridgeAppSettings';
-import React, { ReactNode } from 'react'
-import useSWR from 'swr';
 
 const SettingsContext = React.createContext<BridgeAppSettings | null>(null);
 
 export const SettingsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
 
   const fetcher = (url: string) => fetch(url).then(r => r.json());
-  // const version = process.env.NEXT_PUBLIC_API_VERSION;
-  const version = 'sandbox'
+  const version = AppSettings.ApiVersion;
 
-  const { data: netWorkData } = useSWR<ApiResponse<CryptoNetwork[]>>(`${AppSettings.BridgeApiUri}/api/networks?version=${version}`, fetcher, { dedupingInterval: 60000 })
-  const { data: exchangeData } = useSWR<ApiResponse<CryptoNetwork[]>>(`${AppSettings.BridgeApiUri}/api/exchanges?version=${version}`, fetcher, { dedupingInterval: 60000 })
+  const { data: netWorkData } = useSWR<ApiResponse<CryptoNetwork[]>>(`${AppSettings.BridgeApiUri}/networks?version=${version}`, fetcher, { dedupingInterval: 60000 })
+  const { data: exchangeData } = useSWR<ApiResponse<CryptoNetwork[]>>(`${AppSettings.BridgeApiUri}/exchanges?version=${version}`, fetcher, { dedupingInterval: 60000 })
 
   const settings = {
     networks: netWorkData?.data,
     exchanges: exchangeData?.data,
   }
+
+  console.log("settings::::::", settings)
 
   let appSettings = new BridgeAppSettings(settings);
 
