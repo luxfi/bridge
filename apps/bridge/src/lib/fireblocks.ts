2a69816ddcfaf847d24839fbd4f1e3335a31606a
venuswhispers <venuswhisperer412@gmail.com>
2024-11-13 12:57:31 -0500

    update: networks and exchanges apis

--- apps/bridge/src/lib/fireblocks.ts
+++ apps/bridge/src/lib/fireblocks.ts
@@ -11,7 +11,7 @@ interface FireblocksConfig {
 }
 
 export function getFireblocksConfig(): FireblocksConfig {
-  const isTestnet = process.env.NEXT_PUBLIC_IS_TESTNET === 'true';
+  const isTestnet = process.env.NEXT_PUBLIC_API_VERSION === 'testnet';
 
   return {
     apiKey: isTestnet
