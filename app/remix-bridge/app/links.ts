9960690d92e98ab79f75c362ee3169d568a63e5f
artem ash <artemisprimedev@gmail.com>
2024-11-16 11:14:13 -0800

    minor

--- app/remix-bridge/app/links.ts
+++ app/remix-bridge/app/links.ts
@@ -1,5 +1,7 @@
 import { type LinkDescriptor } from '@remix-run/node'
 
+type CrossOrigin = "anonymous" | "use-credentials"
+
   // https://github.com/remix-run/remix/discussions/4963#discussioncomment-4556820
 export default [
   {
@@ -7,42 +9,42 @@ export default [
     as: "font",
     href: "/fonts/Druk-Wide-Medium.ttf", 
     type: "font/ttf",
-    crossOrigin: "anonymous" as ("anonymous" | "use-credentials")
+    crossOrigin: "anonymous" as CrossOrigin
   },
   {
     rel: "preload",
     as: "font",
     href: "/fonts/Druk-Wide-Bold.ttf", 
     type: "font/ttf",
-    crossOrigin: "anonymous" as ("anonymous" | "use-credentials"),
+    crossOrigin: "anonymous" as CrossOrigin,
   },
   {
     rel: "preload",
     as: "font",
     href: "/fonts/InterVariable.woff2", 
     type: "font/woff2",
-    crossOrigin: "anonymous" as ("anonymous" | "use-credentials"),
+    crossOrigin: "anonymous" as CrossOrigin,
   },
   {
     rel: "preload",
     as: "font",
     href: "/fonts/InterVariable.ttf", 
     type: "font/ttf",
-    crossOrigin: "anonymous" as ("anonymous" | "use-credentials"),
+    crossOrigin: "anonymous" as CrossOrigin,
   },
   {
     rel: "preload",
     as: "font",
     href: "/fonts/InterVariable-Italic.woff2", 
     type: "font/woff2",
-    crossOrigin: "anonymous" as ("anonymous" | "use-credentials"),
+    crossOrigin: "anonymous" as CrossOrigin,
   },
   {
     rel: "preload",
     as: "font",
     href: "/fonts/InterVariable-Italic.ttf", 
     type: "font/ttf",
-    crossOrigin: "anonymous" as ("anonymous" | "use-credentials"),
+    crossOrigin: "anonymous" as CrossOrigin,
   },
   {
     rel: 'icon',
