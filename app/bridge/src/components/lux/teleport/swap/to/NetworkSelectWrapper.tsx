323aaae2c73d931aebad5b9db0ff78f30cc6bded
Artem Ash <633467+artemis-prime@users.noreply.github.com>
2024-11-20 17:42:18 -0800

    Swap widget ui cleanups (#219)
    made teleport and fireblocks match in look and functionality on 
    cleaned up appearance of Swap widget

--- app/bridge/src/components/lux/teleport/swap/to/NetworkSelectWrapper.tsx
+++ app/bridge/src/components/lux/teleport/swap/to/NetworkSelectWrapper.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import NetworkSelect from './NetworkSelect'
 import type { Network } from '@/types/teleport'
 import { ChevronDown } from 'lucide-react'
+import { cn } from '@hanzo/ui/util'
 
 export default function NetworkSelectWrapper<T>({
   network,
@@ -35,54 +36,56 @@ export default function NetworkSelectWrapper<T>({
     }
   }, [])
 
-  return (
-    <>
-      <div className="flex items-center relative">
-        <button
-          type="button"
-          onClick={openModal}
-          disabled={disabled}
-          className={`rounded-lg focus-peer:ring-primary focus-peer:border-muted-3 focus-peer:border focus-peer:ring-1 focus:outline-none disabled:cursor-not-allowed relative grow h-12 flex items-center text-left justify-bottom w-full pl-3 pr-4 py-2 border border-muted-3 font-semibold ${className}`}
-        >
-          <span className="flex grow text-left items-center text-xs md:text-base">
-            {network && (
-              <div className="flex items-center">
-                <div className="flex-shrink-0 h-6 w-6 relative">
-                  <Image
-                    src={network.logo}
-                    alt="Project Logo"
-                    height="40"
-                    width="40"
-                    loading="eager"
-                    priority
-                    className="rounded-md object-contain"
-                  />
-                </div>
-              </div>
-            )}
-            {network ? (
-              <span className="ml-3 block font-medium text-muted flex-auto items-center">
-                {network.display_name}
-              </span>
-            ) : (
-              <span className="block font-medium text-muted-2 flex-auto items-center">
-                {placeholder}
-              </span>
-            )}
+  return (<>
+    <button
+      type="button"
+      onClick={openModal}
+      disabled={disabled}
+      className={cn(
+        'focus-peer:ring-primary', 
+        'focus-peer:border-muted-3 focus-peer:border focus-peer:ring-1 focus:outline-none', 
+        'disabled:cursor-not-allowed relative grow h-12 flex items-center text-left', 
+        'justify-bottom w-full pl-3 pr-4 py-2 border-b border-muted-3 font-semibold', 
+        className
+      )}
+    >
+      <span className="flex grow text-left items-center text-xs md:text-base">
+        {network && (
+          <div className="flex items-center">
+            <div className="flex-shrink-0 h-6 w-6 relative">
+              <Image
+                src={network.logo}
+                alt="Project Logo"
+                height="40"
+                width="40"
+                loading="eager"
+                priority
+                className="rounded-md object-contain"
+              />
+            </div>
+          </div>
+        )}
+        {network ? (
+          <span className="ml-3 block font-medium text-muted flex-auto items-center">
+            {network.display_name}
           </span>
-          <span className="ml-3 right-0 flex items-center pointer-events-none">
-            <ChevronDown className="h-4 w-4" aria-hidden="true" />
+        ) : (
+          <span className="block font-medium text-muted-2 flex-auto items-center">
+            {placeholder}
           </span>
-        </button>
-      </div>
-      <NetworkSelect
-        setShow={setShowModal}
-        setNetwork={handleSelect}
-        show={showModal}
-        network={network}
-        searchHint=""
-        networks={networks}
-      />
-    </>
-  )
+        )}
+      </span>
+      <span className="ml-3 right-0 flex items-center pointer-events-none">
+        <ChevronDown className="h-4 w-4" aria-hidden="true" />
+      </span>
+    </button>
+    <NetworkSelect
+      setShow={setShowModal}
+      setNetwork={handleSelect}
+      show={showModal}
+      network={network}
+      searchHint=""
+      networks={networks}
+    />
+  </>)
 }
