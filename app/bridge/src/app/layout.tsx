fd18715d2b7986b7fe06b859ab91624fef4cd6a1
artem ash <artemisprimedev@gmail.com>
2024-11-26 13:45:09 -0800

    moved connect button into header code

--- app/bridge/src/app/layout.tsx
+++ app/bridge/src/app/layout.tsx
@@ -1,9 +1,8 @@
 import React, { type PropsWithChildren } from 'react'
-import dynamic from "next/dynamic"
 
 import {
   RootLayout as RootLayoutCore,
-  viewport as ViewportCode,
+  //viewport as ViewportCode,
 } from '@luxfi/ui/root-layout'
 
 import { Footer } from '@luxfi/ui'
@@ -18,35 +17,16 @@ import _metadata from '@/metadata'
 import Contexts from '@/components/Contexts'
 import Main from '@/components/main'
 import Header from '@/components/header'
-import { ChevronDown } from 'lucide-react'
+
 
 export const metadata = { ..._metadata }
 // export const viewport = { ...ViewportCode }
-
-const ConnectedWallets = dynamic(
-  () => import("../components/ConnectedWallets").then((comp) => (comp.ConnectedWallets)), 
-  { loading: () => (null) }
-)
-
-
 
 
 const RootLayout: React.FC<PropsWithChildren> = async ({ children }) => (
   <RootLayoutCore siteDef={siteDef} showHeader={false}>
     <Contexts>
-      <Header siteDef={siteDef} logoVariant='logo-only'>
-        <ConnectedWallets 
-          connectButtonVariant='outline' 
-          showWalletIcon={false}
-          connectButtonClx='pl-3 pr-2 flex rounded-lg relative'
-        >
-          <span className='pr-1.5'>Connect</span>
-          <span className='pr-1'>|</span>
-          <span className=''>
-            <ChevronDown className="h-4 w-4" aria-hidden="true" />
-          </span>
-        </ConnectedWallets >
-      </Header>
+      <Header siteDef={siteDef} logoVariant='logo-only' />
       <Main>{children}</Main>
     </Contexts>
     <Footer siteDef={siteDef} />
