634d45ee3d7bcfc700a918d73946458f9ce1ad5f
venuswhispers <venuswhisperer412@gmail.com>
2024-11-26 16:16:30 +0800

    fixed: tx timestamp issue

--- app/bridge/src/store/utila.ts
+++ app/bridge/src/store/utila.ts
@@ -1,19 +1,21 @@
-import { atom } from "jotai";
-import type { Network, Token } from "@/types/teleport";
+import { atom } from 'jotai'
+import type { Network, Token } from '@/types/teleport'
+import type { DepositAction } from '@/types/utila'
 
-export const sourceNetworkAtom = atom<Network | undefined>(undefined);
-export const timeToExpireAtom = atom<number>(0);
-export const sourceAssetAtom = atom<Token | undefined>(undefined);
-export const destinationNetworkAtom = atom<Network | undefined>(undefined);
-export const destinationAssetAtom = atom<Token | undefined>(undefined);
-export const destinationAddressAtom = atom<string>("");
-export const sourceAmountAtom = atom<string>("");
-export const ethPriceAtom = atom<number>(0);
-export const swapStatusAtom = atom<string>("");
-export const swapIdAtom = atom<string>("");
-export const userTransferTransactionAtom = atom<string>("");
-export const bridgeMintTransactionAtom = atom<string>("");
-export const mpcSignatureAtom = atom<string>("");
-export const useTelepoterAtom = atom<boolean>(true);
+export const sourceNetworkAtom = atom<Network | undefined>(undefined)
+export const timeToExpireAtom = atom<number>(0)
+export const sourceAssetAtom = atom<Token | undefined>(undefined)
+export const destinationNetworkAtom = atom<Network | undefined>(undefined)
+export const destinationAssetAtom = atom<Token | undefined>(undefined)
+export const destinationAddressAtom = atom<string>('')
+export const sourceAmountAtom = atom<string>('')
+export const ethPriceAtom = atom<number>(0)
+export const swapStatusAtom = atom<string>('')
+export const swapIdAtom = atom<string>('')
+export const userTransferTransactionAtom = atom<string>('')
+export const bridgeMintTransactionAtom = atom<string>('')
+export const mpcSignatureAtom = atom<string>('')
+export const useTelepoterAtom = atom<boolean>(true)
+export const depositActionsAtom = atom<DepositAction[]>([])
 
-export const depositAddressAtom = atom<undefined|string>(undefined);
+export const depositAddressAtom = atom<undefined|string>(undefined)
