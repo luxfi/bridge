8c5bc3b74e2fdb80330237f4976ba10893b70ea1
venuswhispers <venuswhisperer412@gmail.com>
2024-11-30 18:16:17 +0800

    fixed: settings issues

--- app/bridge/src/components/SwapHistory/index.tsx
+++ app/bridge/src/components/SwapHistory/index.tsx
@@ -252,7 +252,6 @@ function TransactionsHistory() {
                           (n: CryptoNetwork) =>
                             n.internal_name === swap.source_network
                         )
-                        console.log(sourceNetwork)
                         const destinationNetwork = networks.find(
                           (n: CryptoNetwork) =>
                             n.internal_name === swap.destination_network
