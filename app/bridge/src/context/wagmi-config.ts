def3cf4c361d18760c23dfb081168866d839b4cd
artem ash <artemisprimedev@gmail.com>
2024-12-02 16:25:24 -0800

    temp hack so wagmi config compiles

--- app/bridge/src/context/wagmi-config.ts
+++ app/bridge/src/context/wagmi-config.ts
@@ -1,30 +1,21 @@
 import { getDefaultConfig } from "@rabby-wallet/rabbykit";
 import { createClient, type Chain } from "viem";
 import { createConfig, http } from "wagmi";
-import resolveChain from '@/lib/resolveChain'
-import { NetworkType } from '@/Models/CryptoNetwork'
-
-
-import mainnets from '@/settings/mainnet/networks.json'
-import testnets from '@/settings/testnet/networks.json'
-
-//const { wallets } = getDefaultWallets()
-
-const isChain = (c: Chain | undefined): c is Chain => (c != undefined)
-const chains = [...mainnets as Chain[], ...testnets as Chain[]]
-  .filter((n) => (n.type === NetworkType.EVM))
-  .sort((a, b) => (Number(a.chain_id) - Number(b.chain_id)))
-  .map(resolveChain)
-  .filter(isChain)
+import { goerli, mainnet } from "wagmi/chains";
 
 
 const configParams = getDefaultConfig({
   projectId: 'e89228fed40d4c6e9520912214dfd68b',
   appName: "Lux Bridge",
-  chains: [chains as Chain[]] as readonly [Chain[]],
+    // @ts-ignore
+  chains: [
+    mainnet,
+    ...(process.env.NODE_ENV === "development" ? [goerli] : []),
+  ],
   client({ chain } : { chain: Chain}) {
     return createClient({ chain, transport: http() });
   },
 });
 
+    // @ts-ignore
 export const config = createConfig(configParams);
