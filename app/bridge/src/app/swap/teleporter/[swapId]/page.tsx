79ee31c5c0dd77fe163b2dbccb4e22f9e9ec57c8
venuswhispers <venuswhisperer412@gmail.com>
2024-12-07 02:46:21 +0800

    chore: code refactoring

--- app/bridge/src/app/swap/teleporter/[swapId]/page.tsx
+++ app/bridge/src/app/swap/teleporter/[swapId]/page.tsx
@@ -4,9 +4,7 @@ import SwapProcess from '@/components/lux/teleport/process'
 const SwapDetails: React.FC<{
   params: { swapId: string }
 }> = ({ params }) => {
-  return (
-    <SwapProcess swapId={params.swapId} className="my-20" />
-  )
+  return <SwapProcess swapId={params.swapId} className="my-20" />
 }
 
 export default SwapDetails
