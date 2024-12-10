988b4c2ee4ad861bc75321d509c97980de591888
venuswhispers <venuswhisperer412@gmail.com>
2024-12-11 07:05:52 +0800

    fixed: payout issue

--- app/bridge/src/store/teleport.ts
+++ app/bridge/src/store/teleport.ts
@@ -1,15 +1,7 @@
 import type { CryptoNetwork, NetworkCurrency } from "@/Models/CryptoNetwork"
 import { atom } from "jotai"
 
-export const sourceNetworkAtom = atom<CryptoNetwork | undefined>(undefined)
-export const sourceAssetAtom = atom<NetworkCurrency | undefined>(undefined)
-export const destinationNetworkAtom = atom<CryptoNetwork | undefined>(undefined)
-export const destinationAssetAtom = atom<NetworkCurrency | undefined>(undefined)
-export const destinationAddressAtom = atom<string>("")
-export const sourceAmountAtom = atom<string>("")
-export const ethPriceAtom = atom<number>(0)
 export const swapStatusAtom = atom<string>("")
-export const swapIdAtom = atom<string>("")
 export const userTransferTransactionAtom = atom<string>("")
 export const bridgeMintTransactionAtom = atom<string>("")
 export const mpcSignatureAtom = atom<string>("")
