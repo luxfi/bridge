ee55bcfdaab4f908ab344f759e1ec31bfb10858e
artem ash <artemisprimedev@gmail.com>
2024-12-18 10:01:34 -0800

    prev

---
+++ app/new-bridge/app/components/receive-card/bridge-label.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@hanzo/ui/util'
+
+import { Bridge } from '@/domain/types'
+
+const BridgeLabel: React.FC<{
+  bridge: Bridge
+  imageSize?: number
+  imageClx?: string
+  className?: string
+}> = ({
+  bridge : { name, logo },
+  imageSize=24,
+  imageClx='',
+  className=''
+}) => (
+  <div className={cn('flex justify-start items-center gap-2', className)}>
+    <img
+      src={logo}
+      alt={name + ' image'}
+      height={imageSize}
+      width={imageSize}
+      loading="eager"
+      className={cn('block', imageClx)}
+    />
+    <span className='block'>{name}</span>
+  </div>
+)
+
+export default BridgeLabel
