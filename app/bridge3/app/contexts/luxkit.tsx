9da42a09cc0c5382e6c6c8052f1f08974ee95021
venuswhispers <venuswhisperer412@gmail.com>
2024-12-30 08:02:54 +0800

    chore: code refactoring

--- app/bridge3/app/contexts/luxkit.tsx
+++ app/bridge3/app/contexts/luxkit.tsx
@@ -1,12 +1,12 @@
 import { createContext, useRef, useState, type PropsWithChildren } from 'react'
 
-import  { type Chain, createClient } from 'viem'
+import { type Chain, createClient } from 'viem'
 import { createConfig as createWagmiConfig, WagmiProvider, http } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import type { Network } from '@luxfi/core'
 
-import resolveChain from '@/lib/resolve-chain'
+import resolveChain from '@/domain/resolve-chain'
 
 const queryClient = new QueryClient()
 
@@ -35,48 +35,47 @@ interface LuxkitInitializer {
 
 export const LuxkitInitializerContext = createContext<LuxkitInitializer | null>(null)
 
-
-export const LuxKitProvider: React.FC<PropsWithChildren> = ({ 
-  children 
+export const LuxKitProvider: React.FC<PropsWithChildren> = ({
+  children
 }) => {
-    // using state vs ref because state forces an update when it changes, by definition.
+  // using state vs ref because state forces an update when it changes, by definition.
   const [config, setConfig] = useState<ReturnType<typeof createConfig> | null>(null)
 
   const init = (networks: Network[]) => {
-    setConfig(createConfig(networks))  
+    setConfig(createConfig(networks))
   }
 
   return (
     <LuxkitInitializerContext.Provider value={init} >
-    { (config) ? (
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </WagmiProvider>
-    ) : (
-      children
-    )} 
+      {config ?
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </WagmiProvider>
+        :
+        children
+      }
     </LuxkitInitializerContext.Provider>
   )
 }
 
 export default LuxKitProvider
 
-  // const config: Config = getDefaultConfig({
-  //   appName: 'Lux Bridge',
-  //   projectId: 'e89228fed40d4c6e9520912214dfd68b',
-  //   wallets: [
-  //     ...wallets,
-  //     {
-  //       groupName: 'Other',
-  //       wallets: [metaMaskWallet],
-  //     },
-  //   ],
-  //   //@ts-expect-error "ignore chain type"
-  //   chains: chains,
-  //   ssr: true,
-  //   autoConnect: true, // Automatically reconnect the wallet
-  // })
+// const config: Config = getDefaultConfig({
+//   appName: 'Lux Bridge',
+//   projectId: 'e89228fed40d4c6e9520912214dfd68b',
+//   wallets: [
+//     ...wallets,
+//     {
+//       groupName: 'Other',
+//       wallets: [metaMaskWallet],
+//     },
+//   ],
+//   //@ts-expect-error "ignore chain type"
+//   chains: chains,
+//   ssr: true,
+//   autoConnect: true, // Automatically reconnect the wallet
+// })
 
 
