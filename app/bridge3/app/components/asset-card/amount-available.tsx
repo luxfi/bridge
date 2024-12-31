c4cd594cd7170848f6f272721a33897f2652e30c
artem ash <artemisprimedev@gmail.com>
2024-12-31 11:42:02 -0800

    Fixed hmr issue (now using link() function in rout.tsx vs import); Start of debounced and animated ReceiveCard; set 1rem to 16px in lux-global-non-next.css

---
+++ app/bridge3/app/components/asset-card/amount-available.tsx
@@ -0,0 +1,62 @@
+import { type PropsWithChildren } from 'react'
+import { 
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger 
+} from '@hanzo/ui/primitives-common'
+
+const TooltipHelper: React.FC<{
+  text: string
+  tooltipClx?: string
+} & PropsWithChildren> = ({
+  children,
+  text,
+  tooltipClx=''
+}) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        {children}
+      </TooltipTrigger>
+      <TooltipContent className={tooltipClx}>
+        {text}
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+)
+
+const formatAvailable = (v: number | null): string => {
+  if (!v) return ''
+  let str = v.toFixed(5)
+  if (v - parseFloat(str) !== 0) {
+    str += '...'
+  }
+  return str
+}
+
+const AmountAvailable: React.FC<{
+  amount?: number
+  fromAssetName: string
+}> = ({
+  amount,
+  fromAssetName
+}) => {
+  
+  const formattedAmount = formatAvailable(amount ?? null)
+  const availableWasRounded = formattedAmount?.endsWith('...')
+  
+  return (
+    <span className='block shrink-0 cursor-default'>
+    {availableWasRounded ? (
+      <TooltipHelper text={`${amount} ${fromAssetName}`} tooltipClx='text-foreground'>
+          <span>{formattedAmount}&nbsp;{fromAssetName}&nbsp;avail</span>
+      </TooltipHelper>
+    ) : (<>
+      <span>{formattedAmount}&nbsp;{fromAssetName}&nbsp;avail</span>  
+    </>)}
+    </span>
+  )
+}
+
+export default AmountAvailable
