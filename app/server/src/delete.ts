30d52a88c33aa16220f70b4a7eaa400b949c8901
venuswhispers <venuswhisperer412@gmail.com>
2025-01-11 13:04:12 -0600

    fixed: bridge2 notification issue

---
+++ app/server/src/delete.ts
@@ -0,0 +1,29 @@
+import { prisma } from "./prisma-instance"
+
+const main = async () => {
+  try {
+    const arvs = process.argv.slice(2)
+    const swapId = arvs[0]
+    if (swapId) {
+      await prisma.depositAction.deleteMany({
+        where: { swap_id: swapId }
+      })
+      // Delete related Transactions
+      await prisma.transaction.deleteMany({
+        where: { swap_id: swapId }
+      })
+      // Delete related Quote
+      await prisma.quote.deleteMany({
+        where: { swap_id: swapId }
+      })
+      // Finally, delete the Swap
+      const deletedSwap = await prisma.swap.delete({
+        where: { id: swapId }
+      })
+    }
+  } catch (err) {
+    //
+  }
+}
+
+main()
